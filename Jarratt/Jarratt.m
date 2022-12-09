e46f19750a68644a9e53ff7f5d23160126a1a025
Wojtasauce <51636941+WojtekGrbs@users.noreply.github.com>
2022-12-09 02:43:35 +0100

    Czemu nie można deltą po prostu
    Nie wiem

---
+++ Jarratt/Jarratt.m
@@ -0,0 +1,47 @@
+function [x, count] = Jarratt(w, x_0, tol)
+    % Funkcja Jarrat(w,x_0,tol) służy określeniu miejsc zerowych podanego
+    % wielomianu przy pomocy metody Jarratt'a, jednocześnie umożliwiająca
+    % maksymalnie 30 iteracji tej metody, w przeciwnym wypadku miejsce
+    % określane jest jako NaN, a liczba iteracji na 31.
+
+    % w: Współczynniki wielomianu, którego miejsc zerowych szukamy
+    % x_0: Początkowe przybliżenie
+    % tol: (Zwykle mała) liczba rzeczywista określająca warunek stopu
+
+    x0 = x_0;
+    x1 = x_0;
+
+    fx0 = Horner(w, x0);
+
+    % Zmienna count zlicza dotychczasowe iteracje
+    count = 0;
+
+    while (count <= 30)
+        
+        % W pierwszym kroku nie nadpisujemy
+        if count ~= 0 
+            x0 = x1;
+            fx0 = Horner(w, x0);
+            if abs(fx0) < tol
+                x = x1;
+            return
+            end 
+        end
+
+        
+        fix0 = HornerDerivative(w, x0);
+
+        if fix0 == 0
+            disp('oj')
+            return
+        end
+        
+        mianownik = HornerDerivative(w, x0 - (1/2)*fx0/fix0);
+        x1 = x0 - fx0/mianownik;
+        count = count + 1;
+    end
+
+    % W przypadku, gdy po przejściu 30 iteracji nie spełniamy warunku stopu
+    count = 31;
+    x = NaN;
+end
