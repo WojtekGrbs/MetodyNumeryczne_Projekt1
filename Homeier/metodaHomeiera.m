a87ca391318435ee2e58c9408fa70c935f386fb0
SawickiK <100801230+SawickiK@users.noreply.github.com>
2022-12-09 23:39:45 +0100

    Update metodaHomeiera.m

--- Homeier/metodaHomeiera.m
+++ Homeier/metodaHomeiera.m
@@ -1,42 +1,44 @@
-function [k,x,w_x] = metodaHomeiera(p, x0, tol, max_iter)
+function [k,x,w_x] = metodaHomeiera(w, x0, tol)
 
-% Wyznaczanie zera wielomianu metodą Halleya'a
-% p - wspolczynniki wielomianu np. p =[3,2,1], czyli w(x) = 3x^2 + 2x + 1
+% Funkcja metodaHomeiera(w,x0,tol) służy określeniu miejsc zerowych podanego
+% wielomianu przy pomocy metody Homeiera, jednocześnie umożliwiająca
+% maksymalnie 30 iteracji tej metody, w przeciwnym wypadku liczba 
+% iteracji ustalana jest na 31.
+%Jako argumenty metoda przyjmuje:
+% p - wspolczynniki wielomianu
 % x0 - początkowe przybliżenie
 % tol - dokładność
 % max_iter - maksymalna ilość iteracji
 % Metoda zwraca:
+% k - liczba wykonanych iteracji
 % x - przybliżenie
-% i - liczba wykonanych iteracji
-% w_x=w(x), gdzie x jest obliczonym przybliżeniem zera w(x)
+% w_x=w(x)
 
+max_iter = 30;
 k = 0;
 dx = tol + 1;
-xpocz = x0;             % wartość początkowa
-
-      % I pochodna wielomianu
 
 while abs(dx) > tol && k <= max_iter
-    w = Horner(p,x0);
-    y0 = x0 - w/HornerDerivative(p, x0);
-    if abs(w) <= tol               % dokładność jest wystarczająca
+    w = Horner(w,x0);
+    y0 = x0 - w/HornerDerivative(w, x0);
+    if abs(w) <= tol               % warunek na dokładność
         x = x0;
         w_x = w;
         return
     end
-    mianownik = 2*HornerDerivative(p, x0)*(HornerDerivative(p, y0));
+    mianownik = 2*HornerDerivative(w, x0)*(HornerDerivative(w, y0));
     
     if mianownik == 0
         disp(' Dzielenie przez zero! ');
         return
     end
     
-    dx = (w*(HornerDerivative(p, x0)+ HornerDerivative(p, y0)))/mianownik;
-    x1 = x0 - dx;        % obliczanie następnego przybliżenia 
-    k = k + 1;           % aktualizacja i
-    x0 = x1;             % aktualizacja x0
+    dx = (w*(HornerDerivative(w, x0)+ HornerDerivative(w, y0)))/mianownik;
+    x1 = x0 - dx;        % obliczanie xk+1
+    k = k + 1;           % aktualizacja liczby iteracji 
+    x0 = x1;             % aktualizacja xk
 end
 
-fprintf('Nie znaleziono rozwiązania w %d iteracjach, zaczynając od %d z wymaganą precyzją wynoszącej: %d \n', max_iter, xpocz, tol);
-k = 31;         % bo zamiar jest taki, aby wszystkie pierwiastki nie znalezione do 30 iteracji były kolorowane na jeden kolor  
+fprintf('Nie znaleziono rozwiązania w %d iteracjach', max_iter);
+k = 31;         %gdy nie znajdziemy w wymaganej liczbie iteracji to kolorujemy na czarno  
 x = NaN;
