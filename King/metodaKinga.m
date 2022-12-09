e4a29b28dde92e4de5cd955b32c505ecf4cb9335
SawickiK <100801230+SawickiK@users.noreply.github.com>
2022-12-09 23:24:25 +0100

    Implementacja metody Kinga

---
+++ King/metodaKinga.m
@@ -0,0 +1,45 @@
+function [k,x,w_x] = metodaKinga(p, x0, tol, max_iter)
+
+% Wyznaczanie zera wielomianu metodą Halleya'a
+% p - wspolczynniki wielomianu np. p =[3,2,1], czyli w(x) = 3x^2 + 2x + 1
+% x0 - początkowe przybliżenie
+% tol - dokładność
+% max_iter - maksymalna ilość iteracji
+% Metoda zwraca:
+% x - przybliżenie
+% i - liczba wykonanych iteracji
+% w_x=w(x), gdzie x jest obliczonym przybliżeniem zera w(x)
+
+k = 0;
+dx = tol + 1;
+xpocz = x0;             % wartość początkowa
+
+      % I pochodna wielomianu
+
+while abs(dx) > tol && k <= max_iter
+    w = Horner(p,x0);
+    y0 = x0 - w/HornerDerivative(p, x0);
+    if abs(w) <= tol               % dokładność jest wystarczająca
+        x = x0;
+        w_x = w;
+        return
+    end
+    mianownik = HornerDerivative(p, x0)*HornerDerivative(p, y0)*(Horner(p,x0))^3;
+    
+    if mianownik == 0
+        disp(' Dzielenie przez zero! ');
+        return
+    end
+    
+    licznik1 = Horner(p,y0)*HornerDerivative(p, x0)*(Horner(p,x0))^3;
+    licznik2 = HornerDerivative(p, y0)*(Horner(p,y0))^3*Horner(p,x0);
+    licznik = licznik1 + licznik2;
+    dx = licznik/mianownik;
+    x1 = y0 - dx;        % obliczanie następnego przybliżenia 
+    k = k + 1;           % aktualizacja i
+    x0 = x1;             % aktualizacja x0
+end
+
+fprintf('Nie znaleziono rozwiązania w %d iteracjach, zaczynając od %d z wymaganą precyzją wynoszącej: %d \n', max_iter, xpocz, tol);
+k = 31;         % bo zamiar jest taki, aby wszystkie pierwiastki nie znalezione do 30 iteracji były kolorowane na jeden kolor  
+x = NaN;
